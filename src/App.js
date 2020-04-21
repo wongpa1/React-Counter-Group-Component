e7e53995929b3c56327894c9506c5f65de307470
PAUL WONG (INFRA-ISD-OOCLL/HKG) <WONGPA@OOCL.COM>
2020-04-21 19:55:15 +0800

    Add Reset function

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import CounterGroup from './CounterGroup';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CounterGroup />
+      </header>
+    </div>
+  );
+}
+
+export default App;
